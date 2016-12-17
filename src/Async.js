5e1f18e64255bca1eddcbd6d1d992e356a7d56d7
Bee Wilkerson <bwilkerson@zulily.com>
2016-12-17 10:18:28 -0800

    fix for js stack in Async when clearable and single select, clearing the selection results in null newValues passed to onChange

--- src/Async.js
+++ src/Async.js
@@ -186,7 +186,8 @@ export default class Async extends Component {
 			options: (isLoading && loadingPlaceholder) ? [] : options,
 			ref: (ref) => (this.select = ref),
 			onChange: (newValues) => {
-				if (this.props.value && (newValues.length > this.props.value.length)) {
+				newValuesExist = typeof newValues !== '' && newValues !== null;
+				if (this.props.value && newValuesExist && newValues.length > this.props.value.length) {
 					this.clearOptions();
 				}
 				this.props.onChange(newValues);
